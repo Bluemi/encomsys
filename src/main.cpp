5baa771a6480c9eb3c1bb5dd5b884ecc51da5878
Bluemi <bruno.schilling@protonmail.ch>
2019-10-15 19:41:01 +0200

    Initial commit
    - build system
    - test system

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "main.hpp"
+
+#include <iostream>
+
+void do_the_thing() {
+	std::cout << "hello world!" << std::endl;
+}
---
+++ src/main.hpp
@@ -0,0 +1,6 @@
+#ifndef __MAIN_CLASS__
+#define __MAIN_CLASS__
+
+void do_the_thing();
+
+#endif
