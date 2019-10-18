1bb7a0c413f4e733d6fcb84f64a109023f60346c
Bluemi <bruno.schilling@protonmail.ch>
2019-10-19 01:14:45 +0200

    idea for relations

--- tests/main_test.cpp
+++ tests/main_test.cpp
@@ -3,10 +3,17 @@
 
 #include "encomsys.hpp"
 
-struct Player {
-	Player(const std::string& name, float x, float y) : name(name), x(x), y(y), v_x(0.f), v_y(0.f) {}
+struct player_name_t {
+	player_name_t() = default;
+	player_name_t(const std::string& name) : name(name) {}
 
 	std::string name;
+
+};
+
+struct position_t {
+	position_t() = default;
+	position_t(const float x, const float y) : x(x), y(y), v_x(0.f), v_y(0.f) {}
 
 	float x;
 	float y;
@@ -15,24 +22,37 @@ struct Player {
 	float v_y;
 };
 
-using ensys = encom::encomsys<Player>;
+template<typename ...Ts>
+struct relation_t {
+	public:
+		relation_t(Ts... ts) : _component_refs(ts...) {}
 
-void print_player(const Player& player) {
-	std::cout << "Player (name=" << player.name << ", x=" << player.x << ", y=" << player.y << ")" << std::endl;
+		std::tuple<encom::ref<Ts>...> _component_refs;
+
+		using instance = std::tuple<std::optional<Ts* const>...>;
+};
+
+struct enemy_relation_t : relation_t<position_t, player_name_t> {};
+struct player_relation_t : relation_t<position_t, player_name_t> {};
+
+using ensys = encom::encomsys<player_relation_t, player_name_t, position_t>;
+
+void print_player(const player_relation_t::instance& player) {
+	std::cout << "Player (name=" << std::get<player_name_t>(player).name << ", x=" << std::get<position_t>(player).x << ", y=" << std::get<position_t>(player).y << ")" << std::endl;
 }
 
-void do_physics(Player& player) {
-	player.x += player.v_x;
-	player.y += player.v_y;
+void do_physics(player_relation_t::instance& player) {
+	player.position.x += player.position.v_x;
+	player.position.y += player.position.v_y;
 
-	player.v_y -= 0.1f; // gravity
+	player.position.v_y -= 0.1f; // gravity
 }
 
 int main() {
 	ensys e;
 
-	encom::ref<Player> bjoern = e.add(Player("björn", 1.0f, 2.3f));
-	e.add(Player("heike", -2.0f, 3.1f));
+	encom::ref<player_relation_t> bjoern = e.add(player_relation_t(e.add(player_name_t("bjoern")), e.add(position_t(1.f, 2.f))));
+	encom::ref<player_relation_t> heike = e.add(player_relation_t(e.add(player_name_t("heike")), e.add(position_t(11.f, 12.f))));
 
 	std::cout << "initial players:" << std::endl;
 	e.for_each(print_player);
